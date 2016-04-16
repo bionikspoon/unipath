95f810c85e57273b6459d3170a7ad4140242b44e
Manu Phatak <bionikspoon@gmail.com>
2016-04-16 15:40:11 -0500

    use module.export, add jsdoc comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-import path from 'path';
+const path = require('path');
 
-export default function (...base) {
-  return (...paths) => path.resolve(path.join(...(base || [ '.' ]), ...(paths || [ '.' ])));
+module.exports = unipath;
+
+/**
+ * Create a path.join partial.
+ *
+ * @param {...string} bases - Base paths
+ * @returns {partial} Path join partial
+ */
+function unipath(...bases) {
+  return partial;
+
+  /**
+   * Resolve base with paths.
+   *
+   * @param {...string} paths - Paths to join to base
+   * @returns {string} Resolved path
+   */
+  function partial(...paths) {
+    const baseList = bases.length ? bases : [ '.' ];
+    const pathList = paths.length ? paths : [ '.' ];
+
+    return path.resolve(path.join(...baseList, ...pathList));
+  }
 }
---
+++ workflow.md
@@ -0,0 +1,21 @@
+# :beginner: Workflow
+
+* Add code to `src/index.js` and tests to `test/index.js`.
+
+* Lint, build and test a project with `npm run build`.
+
+* Build and watch changes in `src/` with `npm run watch`
+
+* Run only tests with `npm run test`.
+
+* Check coverage with `npm run coverage`.
+
+* Generate a TOC for the `CHANGELOG` with `npm run toc`
+
+* Deploy to a remote origin with `npm run deploy`.
+
+* Bump version and publish a package with `npm run major` or `minor/patch`
+
+## Source
+
+* [generator-rise](https://github.com/bucaran/generator-rise)
